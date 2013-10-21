5ac867d3e55e2f0692a746d3ee8dfe96aa16c05b
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-10-20 23:57:44 -0600

    lets put this in a class and run this b in code climate

--- app/simplejira.rb
+++ app/simplejira.rb
@@ -4,41 +4,44 @@ require 'open-uri'
 require 'net/http'
 require 'yaml'
 
-def read_config
-  config = YAML.load_file('config/config.yml')
-  query = YAML.load_file('config/query.yml')
-  api = YAML.load_file('config/api.yml')
+class SimpleJira
+  def read_config
+    config = YAML.load_file('config/config.yml')
+    query = YAML.load_file('config/query.yml')
+    api = YAML.load_file('config/api.yml')
 
-  @username = config['config']['username']
-  @password = config['config']['password']
-  @basic_uri = config['config']['host']+api['api']['api_version']
-  @search_uri = @basic_uri+api['api']['search']
+    @username = config['config']['username']
+    @password = config['config']['password']
+    @basic_uri = config['config']['host']+api['api']['api_version']
+    @search_uri = @basic_uri+api['api']['search']
 
-  @query = query['query']
-end
-
-def get_jira_json(jira_uri,additional_params)
-  uri=URI(URI::encode(jira_uri+additional_params))
-
-  Net::HTTP.start(uri.host, uri.port,
-    :use_ssl => uri.scheme == 'https') do |http|
-
-    request = Net::HTTP::Get.new uri.request_uri
-    request.basic_auth @username, @password
-
-    response = http.request request
-
-    json_response = JSON.parse(response.body)
-    return json_response
+    @query = query['query']
   end
-end
 
-def display_query_result(key_you_want_to_display)
-  read_config
-  @query.each do |query_name,query|
-    total = get_jira_json(@search_uri,query)[key_you_want_to_display]
-    puts "#{query_name} : #{total}"
+  def get_jira_json(jira_uri,additional_params)
+    uri=URI(URI::encode(jira_uri+additional_params))
+
+    Net::HTTP.start(uri.host, uri.port,
+      :use_ssl => uri.scheme == 'https') do |http|
+
+      request = Net::HTTP::Get.new uri.request_uri
+      request.basic_auth @username, @password
+
+      response = http.request request
+
+      json_response = JSON.parse(response.body)
+      return json_response
+    end
   end
+
+  def display_query_result(key_you_want_to_display)
+    read_config
+    @query.each do |query_name,query|
+      total = get_jira_json(@search_uri,query)[key_you_want_to_display]
+      puts "#{query_name} : #{total}"
+    end
+  end
+
 end
 
-display_query_result('total')
+SimpleJira.new.display_query_result('total')
