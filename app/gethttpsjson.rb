17ad4ff448252c523bf11a93037ebba68fe79062
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-10-21 00:58:11 -0600

    DRY it up son

---
+++ app/gethttpsjson.rb
@@ -0,0 +1,18 @@
+class GetHttpsJson
+  attr_reader :json
+
+  def initialize(username,password,uri,additional_params=String.new(''))
+    uri=URI(URI::encode(uri+additional_params))
+
+    Net::HTTP.start(uri.host, uri.port,
+      :use_ssl => uri.scheme == 'https') do |http|
+
+      request = Net::HTTP::Get.new uri.request_uri
+      request.basic_auth username, password
+
+      response = http.request request
+
+      @json = JSON.parse(response.body)
+    end
+  end
+end
--- app/simplejira.rb
+++ app/simplejira.rb
@@ -4,7 +4,10 @@ require 'open-uri'
 require 'net/http'
 require 'yaml'
 
+require_relative 'gethttpsjson'
+
 class SimpleJira
+
   def read_config
     config = YAML.load_file('config/config.yml')
     query = YAML.load_file('config/query.yml')
@@ -18,26 +21,11 @@ class SimpleJira
     @query = query['query']
   end
 
-  def get_jira_json(jira_uri,additional_params)
-    uri=URI(URI::encode(jira_uri+additional_params))
-
-    Net::HTTP.start(uri.host, uri.port,
-      :use_ssl => uri.scheme == 'https') do |http|
-
-      request = Net::HTTP::Get.new uri.request_uri
-      request.basic_auth @username, @password
-
-      response = http.request request
-
-      json_response = JSON.parse(response.body)
-      return json_response
-    end
-  end
-
   def display_query_result(key_you_want_to_display)
     read_config
     @query.each do |query_name,query|
-      total = get_jira_json(@search_uri,query)[key_you_want_to_display]
+      json = GetHttpsJson.new(@username,@password,@search_uri,query).json
+      total = json[key_you_want_to_display]
       puts "#{query_name} : #{total}"
     end
   end
