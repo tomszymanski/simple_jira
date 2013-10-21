bdbeb6ec491fd57818057d89b737b55eb3e31c3c
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-10-21 09:27:34 -0600

    A little more DRY

--- app/simplejira.rb
+++ app/simplejira.rb
@@ -5,31 +5,26 @@ require 'net/http'
 require 'yaml'
 
 require_relative 'gethttpsjson'
+require_relative 'readconfig'
 
 class SimpleJira
 
-  def read_config
-    config = YAML.load_file('config/config.yml')
-    query = YAML.load_file('config/query.yml')
-    api = YAML.load_file('config/api.yml')
+  def initialize(key_you_want_to_display='total')
+    config = ReadConfig.new
 
-    @username = config['config']['username']
-    @password = config['config']['password']
-    @basic_uri = config['config']['host']+api['api']['api_version']
-    @search_uri = @basic_uri+api['api']['search']
-
-    @query = query['query']
-  end
-
-  def display_query_result(key_you_want_to_display)
-    read_config
-    @query.each do |query_name,query|
-      json = GetHttpsJson.new(@username,@password,@search_uri,query).json
-      total = json[key_you_want_to_display]
-      puts "#{query_name} : #{total}"
+    config.query.each do |query_name,query|
+      json = GetHttpsJson.new(
+        config.username,
+        config.password,
+        config.search_uri,
+        query
+      ).json
+      value = json[key_you_want_to_display]
+      puts "#{query_name} : #{value}"
     end
+
   end
 
 end
 
-SimpleJira.new.display_query_result('total')
+SimpleJira.new
