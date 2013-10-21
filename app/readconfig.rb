e5b0ce1fed88670b0bfb9c79b7f995b0df780f8a
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-10-21 09:29:31 -0600

    yea, need to add one more file here

---
+++ app/readconfig.rb
@@ -0,0 +1,16 @@
+class ReadConfig
+    attr_reader :username,:password,:basic_uri,:search_uri,:query
+
+    def initialize
+        config = YAML.load_file('config/config.yml')
+        query = YAML.load_file('config/query.yml')
+        api = YAML.load_file('config/api.yml')
+
+        @username = config['config']['username']
+        @password = config['config']['password']
+        @basic_uri = config['config']['host']+api['api']['api_version']
+        @search_uri = @basic_uri+api['api']['search']
+
+        @query = query['query']
+    end
+end
