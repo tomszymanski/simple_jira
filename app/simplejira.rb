d7565e3f57ee6d809756c2074dbe0abf1c0398f0
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-11-22 17:38:39 -0700

    add release notes option

--- app/simplejira.rb
+++ app/simplejira.rb
@@ -1,19 +1,25 @@
 require 'rubygems'
 require 'json'
-require 'open-uri'
 require 'net/http'
+require 'open-uri'
+require 'optparse'
 require 'yaml'
 
 require_relative 'gethttpsjson'
 require_relative 'readconfig'
-require_relative 'valuefromquery'
+require_relative 'jiraquery'
 
-class SimpleJira
-  def initialize
-    simple_jira = ValueFromQuery.new
+simple_jira = JiraQuery.new
+
+case ARGV[0]
+
+  when /^release_notes/i
+    project = ARGV.slice(1..-1)
+    simple_jira.get_issues(project[0].to_s)
+    simple_jira.display_release_notes
+
+  when /^auto_query/i
     simple_jira.get_value
     simple_jira.display_value
-  end
-end
 
-SimpleJira.new
+end
