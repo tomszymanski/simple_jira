007f2f577ce2cc8814144df0e4b812e7c3c37564
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-11-22 22:50:35 -0700

    trying to fix open ssl issue for Ben

--- app/gethttpsjson.rb
+++ app/gethttpsjson.rb
@@ -1,11 +1,12 @@
 class GetHttpsJson
   attr_reader :json
 
-  def initialize(username,password,uri,additional_params=String.new(''))
+  def initialize(username,password,uri,additional_params=String.new)
     uri=URI(URI::encode(uri+additional_params))
 
     Net::HTTP.start(uri.host, uri.port,
-      :use_ssl => uri.scheme == 'https') do |http|
+      :use_ssl => uri.scheme == 'https',
+      :openssl_verify_mode => 'none') do |http|
 
       request = Net::HTTP::Get.new uri.request_uri
       request.basic_auth username, password
